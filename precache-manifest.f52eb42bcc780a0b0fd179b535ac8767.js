fb7387fff9b319227eece4b974d8e778a8389fc8
David Delgado <davidadelgado08@gmail.com>
2022-11-18 09:54:20 -0600

    Updates

---
+++ precache-manifest.f52eb42bcc780a0b0fd179b535ac8767.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e146eb1800e635c7cc2a",
+    "url": "/portfolio/static/css/main.91e66953.chunk.css"
+  },
+  {
+    "revision": "e146eb1800e635c7cc2a",
+    "url": "/portfolio/static/js/main.e146eb18.chunk.js"
+  },
+  {
+    "revision": "31088909e753e45fc079",
+    "url": "/portfolio/static/js/1.31088909.chunk.js"
+  },
+  {
+    "revision": "a76ab6de464730ac9b86",
+    "url": "/portfolio/static/js/2.a76ab6de.chunk.js"
+  },
+  {
+    "revision": "a6104d4b507060f990fc",
+    "url": "/portfolio/static/js/runtime~main.a6104d4b.js"
+  },
+  {
+    "revision": "2cbe464d0c3e3ecb40a11653c3141419",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.d6afe03dac54cb3d1b4fff4f72893503.js"
+  "/portfolio/precache-manifest.f52eb42bcc780a0b0fd179b535ac8767.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
