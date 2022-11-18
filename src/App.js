3620e35ffd8309bc22b2c03bbe13ae56fa258460
David Delgado <davidadelgado08@gmail.com>
2022-11-18 10:56:09 -0600

    Revert "Updates"
    This reverts commit 9bc194d66649613602098c6c683dc81b22831031.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Banner from "./componets/Banner/Banner.tsx";
+
+function App() {
+  return (
+    <div className="Main">
+      <Banner></Banner>
+    </div>
+  );
+}
+
+export default App;
